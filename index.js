9b1d1e3ea80d4b8b2197aac1afc74eadaecb9d26
TonyXia2001 <summertony717@gmail.com>
2021-08-08 16:03:45 -0700

    main page draft 1

---
+++ index.js
@@ -0,0 +1,35 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const data = {
+    labels: [
+        'Image MCQ',
+        'Text MCQ',
+        'Fill-in-the-blank'
+    ],
+    datasets: [{
+        label: 'Task Distr.',
+        data: [57672, 31578, 18189],
+        backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+    }]
+};
+const config = {
+    type: 'pie',
+    data: data,
+};
+var myChart = new Chart(
+    document.getElementById('task-pie-chart'),
+    config
+);
